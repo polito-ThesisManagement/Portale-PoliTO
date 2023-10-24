adfb77e3b0cef205df1adfd2426359bd68322980
Gian114 <100781136+Gian114@users.noreply.github.com>
2023-10-24 16:13:56 +0200

    contnuato orario lezioni in esami

--- src/components/Timetable.js
+++ src/components/Timetable.js
@@ -3,92 +3,62 @@ import Courses from '../Courses.json'
 import Table from 'react-bootstrap/Table';
 
 import '../App.css'
+import { Container } from 'react-bootstrap';
+import Col from 'react-bootstrap/Col';
 
 const giorniSettimana = ['Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì'];
+const orari = ['8:30', '10:00', '11:30', '13:00', '14:30', '16:00', '17:30', '19:00'];
 
 export default function Timetable() {
     return (
-        <Table bordered className='Table'>
-            <thead>
-                <tr>
-                    <th></th>
-                    {giorniSettimana.map((giorno) => {
-                        return (<th key={giorno}>{giorno}</th>)
+        <Container className='my-5'>
+            <Table responsive size='md' borderless>
+                <thead>
+                    <tr>
+                        <th></th>
+                        {giorniSettimana.map((giorno) => {
+                            return (<th style={{backgroundColor : '#03577A', color: '#fff'}} key={giorno}>{giorno}</th>)
+                        })}
+                    </tr>
+                </thead>
+                <tbody>
+                    {orari.map((orario, index) => {
+                        return (
+                            <tr key={index}>
+                                <td style={{ width: '5%', height: '70px', backgroundColor: '#FFC300', color: '#fff', fontWeight: 'bold', textAlign: 'center' }}>
+                                    {orario}
+                                </td>
+                                {giorniSettimana.map((giorno, indice) => {
+                                    const filteredCourses = Courses.filter((corso) => {
+                                        return (
+                                            corso.orarioSettimanale.some(obj =>
+                                                obj.giorno === giorniSettimana[indice] && obj.orario.substring(0, 5).includes(orario))
+                                            //corso.orarioSettimanale.giorno === giorniSettimana[indice] 
+                                            //aggiungi parte filtri
+                                        );
+                                    });
+                                    return (
+                                        <td key={indice}>
+                                            <Container style={{display: 'flex', position: 'relative'}}>
+                                                    {filteredCourses.map((corso, courseIndex) => (
+                                                        <Col md={2 /* si puù aumentare numero corso sovrapponibili */} key={courseIndex}
+                                                        className='col-6'>
+                                                            <div
+                                                                key={courseIndex}
+                                                                className={`course-bar bg-${corso.colore}`}
+                                                                title={`${corso.nome} ${corso.docente}`}
+                                                            ></div>
+                                                        </Col>
+                                                    ))}
+                                            </Container>
+                                        </td>
+                                    );
+                                })}
+                            </tr>
+                        );
                     })}
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>8:30</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>10:00</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>11:30</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>13:00</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>14:30</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>16:00</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>17:30</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>19:00</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-            </tbody>
-
-
-
-
-
-        </Table>
-
+                </tbody>
+            </Table>
+        </Container>
     );
 }
