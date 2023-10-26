385ba2471e1388c94a068ae8da188f04208b2835
giacomo13 <100355379+giacomo13@users.noreply.github.com>
2023-10-26 15:23:24 +0200

    Sidebar working

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import './App.css';
 import PoliNavbar from './components/Navbar';
 import Libretto from './pages/Libretto';
 import Tesi from './pages/Tesi';
+import Home from './pages/Home';
+import { Row, Col } from 'react-bootstrap';
 import { Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <>
-    <PoliNavbar />
-    <Routes>
-      <Route path='/' element={<Libretto />}></Route>
-      <Route path='/libretto' element={<Libretto />}></Route>
-      <Route path='/tesi' element={<Tesi />}></Route>
-    </Routes>
+      <PoliNavbar />
+      <Row>
+        <Sidebar/>
+        <Col md={10}>
+          <Routes>
+            <Route path='/' element={<Libretto />} />
+            <Route path='/libretto' element={<Libretto />} />
+            <Route path='/tesi' element={<Tesi />} />
+            <Route path='/home' element={<Home />} />
+          </Routes>
+        </Col>
+      </Row>
     </>
   );
 }
