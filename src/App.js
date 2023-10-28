c887257153e1ee12355753a82c3e464895cb1593
Gian114 <100781136+Gian114@users.noreply.github.com>
2023-10-28 14:05:56 +0200

    Committed Navigation inside course

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import { Row, Col } from 'react-bootstrap';
+import { Route, Routes } from 'react-router-dom';
+
+import './App.css';
+
 import PoliNavbar from './components/Navbar';
 import Libretto from './pages/Libretto';
 import Servizi from './pages/Servizi';
@@ -6,10 +10,12 @@ import Tesi from './pages/Tesi';
 import Home from './pages/Home';
 import Help from './pages/Help';
 import Timetable from './components/Timetable'
-import { Route, Routes } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import CoursePage from './pages/CoursePage';
-import './App.css';
+import Materiale from './pages/course_sections/Materiale';
+import Avvisi from './pages/course_sections/Avvisi';
+import CPD from './pages/course_sections/CPD';
+
 
 
 //metterei il path di Home solo con "/"
@@ -28,7 +34,11 @@ function App() {
             <Route path='/servizi' element={<Servizi/>}/>
             <Route path='/help' element={<Help/>} />
             <Route path='/orario' element={<Timetable />} />
-            <Route path="/didattica/:nome" element={<CoursePage />} />
+            <Route path="/didattica/:nome" element={<CoursePage />}>
+              <Route path="materiale" element={<Materiale />} />
+              <Route path="avvisi" element={<Avvisi />} />
+              <Route path="cpd" element={<CPD />} />
+            </Route>
           </Routes>
         </Col>
       </Row>
---
+++ src/pages/course_sections/Avvisi.js
@@ -0,0 +1,10 @@
+export default function Avvisi() {
+
+    //non presente nella nuova architettura, se elimi la pagina elimina anche il tutto su Data.json 
+    return (
+        <>
+            <h1>Avvisi</h1>
+        </>
+    );  
+
+}
