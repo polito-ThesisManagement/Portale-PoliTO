b59b91f7cdbf4b91873197f154b45dfdd2454f8f
giacomo13 <100355379+giacomo13@users.noreply.github.com>
2023-10-28 12:27:23 +0200

    update

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,29 +1,31 @@
 import React from 'react';
-import {Nav, Col} from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import {Nav, Col, Container} from 'react-bootstrap';
+import { Link, useLocation } from 'react-router-dom';
 import "../styles/Sidebar.css"
 
 export default function Sidebar() {
+  const location = useLocation();
+
   return (
-    <Col className="col-md-2 d-none d-md-block bg-light custom-sidebar">
+    <Col className="col-md-2 d-none d-md-block bg-light custom-sidebar px-4 py-1">
       <Nav defaultActiveKey="/home" className="flex-column">
         <Nav.Item>
-          <Link to="/home" className="nav-link">Home</Link>
+          <Link to="/home" className={`nav-link ${location.pathname === '/home' ? 'active' : ''}`}>Home</Link>
         </Nav.Item>
         <Nav.Item>
-          <Link to="/libretto" className="nav-link">Libretto</Link>
+          <Link to="/libretto" className={`nav-link ${location.pathname === '/libretto' ? 'active' : ''}`}>Libretto</Link>
         </Nav.Item>
         <Nav.Item>
-          <Link to="/tesi" className="nav-link">Tesi</Link>
+          <Link to="/tesi" className={`nav-link ${location.pathname === '/tesi' ? 'active' : ''}`}>Tesi</Link>
         </Nav.Item>
         <Nav.Item>
-          <Link to="/esami" className="nav-link">Esami</Link>
+          <Link to="/esami" className={`nav-link ${location.pathname === '/esami' ? 'active' : ''}`}>Esami</Link>
         </Nav.Item>
         <Nav.Item>
-          <Link to="/servizi" className="nav-link">Servizi</Link>
+          <Link to="/servizi" className={`nav-link ${location.pathname === '/servizi' ? 'active' : ''}`}>Servizi</Link>
         </Nav.Item>
         <Nav.Item>
-          <Link to="/help" className="nav-link">Help</Link>
+          <Link to="/help" className={`nav-link ${location.pathname === '/help' ? 'active' : ''}`}>Help</Link>
         </Nav.Item>
       </Nav>
     </Col>
