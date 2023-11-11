4c8e4219e514b225d6b2b4caa04542e3bc43c24c
giacomo13 <100355379+giacomo13@users.noreply.github.com>
2023-11-11 15:22:58 +0100

    didattica started, fixed content and coruses

--- src/pages/didattica/Corsi.js
+++ src/pages/didattica/Corsi.js
@@ -1,44 +1,80 @@
-import { Container, ListGroup } from 'react-bootstrap';
-import { ListUl, JournalText, Upload } from 'react-bootstrap-icons';
+import { Row, Col, Container, ListGroup } from 'react-bootstrap';
 
 import Courses from '../../data/Courses.json'
+import AltriCorsi from '../../data/AltriCorsi.json'
 import CourseSummary from '../../components/CourseSummary';
 
-import '../../styles/App.css';
+import { FaList } from 'react-icons/fa';
+import { PiListPlusFill, PiVideoCameraFill } from 'react-icons/pi'
+import { BsCalendarCheckFill } from 'react-icons/bs'
+import { ImFolderUpload } from 'react-icons/im'
 
 export default function Corsi() {
     //occhio alla navigazione dentro i corsi, per il momento in altri e in carico didattico ci sono gli stessi corsi
     return (
         <>
-            <Container className='mt-4'>
-                <div className='mt-2' style={{ display: 'flex' }}>
-                    <ListUl size={25} className='me-2 mt-2' />
-                    <h2 className='text-style'>Carico Didattico</h2>
-                </div>
-                <ListGroup className="me-2">
-                    {Courses.map((corso) => {
-                        return (
-                            <CourseSummary key={corso.codice} codice={corso.codice} nome={corso.nome} periodo={corso.periodo} crediti={corso.crediti} />
-                        )
-                    })}
-                </ListGroup>
-                <div className='mt-2' style={{ display: 'flex' }}>
-                    <JournalText size={25} className='me-2 mt-2' />
-                    <h2 className='text-style'>Altri Corsi</h2>
-                </div>
-                <ListGroup className="me-2">
-                    {Courses.map((corso) => {
-                        return (
-                            <CourseSummary key={corso.codice} codice={corso.codice} nome={corso.nome} periodo={corso.periodo} crediti={corso.crediti} />
-                        )
-                    })}
-                </ListGroup>
-                <div className='mt-2' style={{ display: 'flex' }}>
-                    <Upload size={25} className='me-2 mt-2' />
-                    <h2 className='text-style'>Materiale Condiviso</h2>
-                </div>
-
-            </Container>
+            <Row>   
+                <Col sm={6}>
+                    <Container className='custom-container'>
+                        <div className="subsection">
+                            <span className="subsection-title">
+                                <FaList size={20} className='subsection-icon' />
+                                Carico didattico
+                            </span>
+                        </div>
+                        <ListGroup>
+                            {Courses.map((corso) => {
+                                return (
+                                    <CourseSummary key={corso.codice} codice={corso.codice} nome={corso.nome} periodo={corso.periodo} crediti={corso.crediti} />
+                                )
+                            })}
+                        </ListGroup>
+                    </Container>
+                    <Container className='custom-container'>
+                    <div className="subsection">
+                            <span className="subsection-title">
+                                <PiListPlusFill size={20} className='subsection-icon' />
+                                Altri corsi
+                            </span>
+                        </div>
+                        <ListGroup>
+                            {AltriCorsi.map((corso) => {
+                                return (
+                                    <CourseSummary key={corso.codice} codice={corso.codice} nome={corso.nome} periodo={corso.periodo} crediti={corso.crediti} />
+                                )
+                            })}
+                        </ListGroup>
+                    </Container>
+                </Col> 
+                <Col sm={6}>
+                    <Container className='custom-container'>
+                        <div className="subsection">
+                            <span className="subsection-title">
+                                <BsCalendarCheckFill size={20} className='subsection-icon' />
+                                Appelli prenotati
+                            </span>
+                        </div>
+                    </Container>
+                    <Container className='custom-container'>
+                        <div className="subsection">
+                            <span className="subsection-title">
+                                <PiVideoCameraFill size={20} className='subsection-icon' />
+                                Virtual classroom recenti
+                            </span>
+                        </div>
+                        
+                    </Container>
+                    <Container className='custom-container'>
+                        <div className="subsection">
+                            <span className="subsection-title">
+                                <ImFolderUpload size={20} className='subsection-icon' />
+                                Materiale condiviso
+                            </span>
+                        </div>
+                        
+                    </Container>
+                </Col>                 
+            </Row>
         </>
     );  
 
