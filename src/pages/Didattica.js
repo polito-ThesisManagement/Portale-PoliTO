9a2137d907afcf8469b5f17f8cf37d986bcffa19
giacomo13 <100355379+giacomo13@users.noreply.github.com>
2023-11-11 13:50:16 +0100

    didattica buttons updated

--- src/pages/Didattica.js
+++ src/pages/Didattica.js
@@ -1,10 +1,7 @@
-import '../styles/App.css'
-
-import { Container } from 'react-bootstrap';
-import { Book, JournalCheck, ClockFill, Translate, ListTask } from 'react-bootstrap-icons';
-
+import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 import { Link, Outlet, useLocation } from 'react-router-dom';
+import { Book, JournalCheck, ClockFill, Translate, ListTask } from 'react-bootstrap-icons';
 
 
 //TODO: modificare font in bottone per ora c'è solo colore del poli
@@ -12,39 +9,51 @@ export default function Didattica() {
 
     const location = useLocation();
     const currentPath = location.pathname;
-
     return (
         <>
             <div className="title">
                 <Book size={28} />
-                <span className="section-title" style={{marginLeft:'5px', marginTop:'3px'}}>Didattica</span>
+                <span className="section-title" style={{marginLeft:'10px', marginTop:'3px'}}>Didattica</span>
             </div>
-                <div className='my-3'>
-                    <Button
-                        className={`didattica-button ${currentPath === '/didattica' ? 'active' : ''} text-style`}
-                        style={{ marginRight: '5px' }}
-                        as={Link} to="/didattica"
-                    >
-                        <ListTask /> I miei corsi
-                    </Button>
-                    <Button
-                        className={`didattica-button ${currentPath === '/didattica/libretto' ? 'active' : ''} text-style`}
-                        style={{ marginRight: '5px' }}
-                        as={Link} to="libretto">
-                        <JournalCheck /> Libretto
-                    </Button>
-                    <Button
-                        className={`didattica-button ${currentPath === '/didattica/orario' ? 'active' : ''} text-style`}
-                        style={{ marginRight: '5px' }}
-                        as={Link} to="orario">
-                        <ClockFill /> Orario Lezioni
-                    </Button>
-                    <Button
-                        className={`didattica-button ${currentPath === '/didattica/lingue' ? 'active' : ''} text-style`}
-                        as={Link} to="lingue">
-                        <Translate /> Lingue
-                    </Button>
-                </div>
+
+            <Row style={{marginLeft:'8px'}}>
+                <Button
+                    className={`custom-button ${currentPath === '/didattica' ? 'active' : ''}`}
+                    style={{ width: '200px', marginRight: '48px', display: 'flex', alignItems: 'center' }}
+                    as={Link}
+                    to="/didattica"
+                >
+                    <ListTask size={24} />
+                    <span style={{ flex: '1', textAlign: 'center' }}>Corsi</span>
+                </Button>
+                <Button
+                    className={`custom-button ${currentPath === '/didattica/libretto' ? 'active' : ''}`}
+                    style={{ width: '200px', marginRight: '48px', display: 'flex', alignItems: 'center' }}
+                    as={Link}
+                    to="/didattica/libretto"
+                >
+                <JournalCheck size={24} />
+                <span style={{ flex: '1', textAlign: 'center' }}>Libretto</span>
+                </Button>
+                <Button
+                    className={`custom-button ${currentPath === '/didattica/orario' ? 'active' : ''}`}
+                    style={{ width: '200px', marginRight: '48px', display: 'flex', alignItems: 'center' }}
+                    as={Link}
+                    to="/didattica/orario"
+                >
+                    <ClockFill size={20} />
+                    <span style={{ flex: '1', textAlign: 'center' }}>Orario lezioni</span>
+                </Button>
+                <Button
+                    className={`custom-button ${currentPath === '/didattica/lingue' ? 'active' : ''}`}
+                    style={{ width: '200px', marginRight: '48px', display: 'flex', alignItems: 'center' }}
+                    as={Link}
+                    to="/didattica/lingue"
+                >
+                <Translate size={24} />
+                <span style={{ flex: '1', textAlign: 'center' }}>Lingue</span>
+                </Button>
+            </Row>
 
             <Outlet />
 
