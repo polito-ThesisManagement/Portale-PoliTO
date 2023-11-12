933577eae16c49004566ce80b8bd7ec6fa998f26
giacomo13 <100355379+giacomo13@users.noreply.github.com>
2023-11-12 10:47:17 +0100

    week and month view

--- src/components/WidgetCalendar.js
+++ src/components/WidgetCalendar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import events from '../data/Events';
@@ -8,50 +8,55 @@ import { BsChevronRight, BsChevronLeft } from 'react-icons/bs';
 
 
 const CustomToolbar = (toolbar) => {
-    const goToBack = () => {
-      toolbar.onNavigate('PREV');
-    };
-  
-    const goToNext = () => {
-      toolbar.onNavigate('NEXT');
-    };
-  
-    const goToToday = () => {
-      toolbar.onNavigate('TODAY');
-    };
-  
-    const goToWeekView = () => {
-      toolbar.onView('week');
-    };
-  
-    const goToMonthView = () => {
-      toolbar.onView('month');
-    };
-  
-    return (
-      <div className="rbc-toolbar">
-        <span className="rbc-btn-group">
-          <button type="button" onClick={goToBack}>
-            <BsChevronLeft />
+
+  const [isMonthView, setMonthView] = useState(true);
+
+  const goToBack = () => {
+    toolbar.onNavigate('PREV');
+  };
+
+  const goToNext = () => {
+    toolbar.onNavigate('NEXT');
+  };
+
+  const goToToday = () => {
+    toolbar.onNavigate('TODAY');
+  };
+
+  const goToWeekView = () => {
+    setMonthView(false);
+    toolbar.onView('week');
+  };
+
+  const goToMonthView = () => {
+    setMonthView(true);
+    toolbar.onView('month');
+  };
+
+  return (
+    <div className="rbc-toolbar">
+      <span className="rbc-btn-group">
+        <button type="button" onClick={goToBack}>
+          <BsChevronLeft />
+        </button>
+        <button type="button" onClick={goToToday}>
+          Oggi
+        </button>
+        <button type="button" onClick={goToNext}>
+          <BsChevronRight />
+        </button>
+      </span>
+      <span className="rbc-toolbar-label">{toolbar.label}</span>
+      <span className="rbc-btn-group">
+          <button type="button" onClick={goToWeekView} className='view-button'>
+              Settimana
           </button>
-          <button type="button" onClick={goToToday}>
-            Oggi
+          <button type="button" onClick={goToMonthView} className= 'view-button'> 
+              Mese
           </button>
-          <button type="button" onClick={goToNext}>
-            <BsChevronRight />
-          </button>
-        </span>
-        <span className="rbc-toolbar-label">{toolbar.label}</span>
-        <span className="rbc-btn-group">
-            <button type="button" onClick={goToWeekView}  className='change-button'>
-                Settimana
-            </button>
-            <button type="button" onClick={goToMonthView} className='change-button'> 
-                Mese
-            </button>
-        </span>
-      </div>
-    );
+      </span>
+    </div>
+  );
 };
 
 export default function WidgetCalendar() {
@@ -66,7 +71,7 @@ export default function WidgetCalendar() {
         endAccessor="end"
         min={new Date(2023, 10, 12, 8, 0)} // Imposta l'orario minimo a 8:00
         max={new Date(2023, 10, 12, 20, 0)} // Imposta l'orario massimo a 19:00
-         defaultDate={new Date(2023, 10, 12)}
+        defaultDate={new Date(2023, 10, 12)}
         style={{ height: '400px', fontFamily:'Montserrat, sans-serif', marginTop:'16px' }}
         components={{
             toolbar: CustomToolbar,
