afcb58bc42584b1f5d0620a7c05cf1a598ac81a2
giacomo13 <100355379+giacomo13@users.noreply.github.com>
2023-11-16 12:43:27 +0100

    update events

--- src/data/Events.js
+++ src/data/Events.js
@@ -1,20 +1,24 @@
 const events = [
     {
-      title: 'Scedenza',
-      start: new Date(2023, 10, 11, 8, 0),
-      end: new Date(2023, 10, 11, 12, 0),
+      title: 'Lezione 1',
+      start: new Date(2023, 10, 15, 8, 0),
+      end: new Date(2023, 10, 15, 12, 0),
     },
     {
-      title: 'Evento 2',
-      start: new Date(2023, 10, 11, 8, 0),
-      end: new Date(2023, 10, 11, 12, 0),
+      title: 'Lezione 2',
+      start: new Date(2023, 10, 15, 13, 0),
+      end: new Date(2023, 10, 15, 16, 0),
     },
     {
-      title: 'Evento 3',
-      start: new Date(2023, 10, 11, 8, 0),
-      end: new Date(2023, 10, 11, 12, 0),
+      title: 'Lezione 3',
+      start: new Date(2023, 10, 16, 8, 0),
+      end: new Date(2023, 10, 16, 10, 30),
     },
-
+    {
+      title: 'Lezione 4',
+      start: new Date(2023, 10, 17, 13, 0),
+      end: new Date(2023, 10, 17, 14, 30),
+    },
   ];
 
 export default events;
