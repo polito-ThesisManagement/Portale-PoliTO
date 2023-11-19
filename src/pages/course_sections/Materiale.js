333a48beec3ee358c13d284a3d4e413b6647d601
Gian114 <100781136+Gian114@users.noreply.github.com>
2023-11-19 12:27:52 +0100

    folderSystem in materiale

--- src/pages/course_sections/Materiale.js
+++ src/pages/course_sections/Materiale.js
@@ -3,7 +3,10 @@ import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
 
-import { ArrowLeft, ArrowRight, Search, Grid } from 'react-bootstrap-icons';
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
+
+
+import { ArrowLeft, ArrowRight, Search, Grid, FolderFill, Folder2Open, FolderSymlinkFill, Dropbox } from 'react-bootstrap-icons';
 
 export default function Materiale() {
 
@@ -39,7 +42,40 @@ export default function Materiale() {
             <Row>
                 <Col lg={3}>
                     <Container className='custom-container'>
-
+                        <ListGroup>
+                            <ListGroupItem style={{border: 'hidden'}}>
+                                <FolderSymlinkFill size={25} />
+                                <span className='ms-2 text-style' style={{fontWeight: 'bold'}}>Materiale didattico</span>
+                            </ListGroupItem>
+                            <ListGroupItem className='ms-2' style={{border: 'hidden'}}>
+                                <FolderFill size={20} />
+                                <span className='ms-2 text-style'>Casi studio</span>
+                            </ListGroupItem>
+                            <ListGroupItem className='ms-2' style={{border: 'hidden'}}>
+                                <Folder2Open size={20} />
+                                <span className='ms-2 text-style'>Labs</span>
+                            </ListGroupItem>
+                            <ListGroupItem className='ms-4' style={{border: 'hidden', backgroundColor: '#EF7B00', maxWidth: '100px', borderRadius: '10px'}}>
+                                <FolderFill size={20} />
+                                <span className='ms-2 text-style'>Lab1</span>
+                            </ListGroupItem>
+                            <ListGroupItem className='ms-4' style={{border: 'hidden'}}>
+                                <FolderFill size={20} />
+                                <span className='ms-2 text-style'>Lab2</span>
+                            </ListGroupItem>
+                            <ListGroupItem className='ms-2' style={{border: 'hidden'}}>
+                                <FolderFill size={20} />
+                                <span className='ms-2 text-style'>Lezioni</span>
+                            </ListGroupItem>
+                            <ListGroupItem className='ms-2' style={{border: 'hidden'}}>
+                                <FolderFill size={20} />
+                                <span className='ms-2 text-style'>Progetti</span>
+                            </ListGroupItem>
+                            <ListGroupItem style={{border: 'hidden'}}>
+                                <Dropbox size={25} />
+                                <span className='ms-2 text-style' style={{fontWeight: 'bold'}}>Dropbox</span>
+                            </ListGroupItem>
+                        </ListGroup>
 
                     </Container>
                 </Col>
