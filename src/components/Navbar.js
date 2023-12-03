0a2bcc475c1b1564a4b0dd600517ac9474988624
Gian114 <100781136+Gian114@users.noreply.github.com>
2023-12-03 11:54:48 +0100

    Notifiche updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,6 +7,8 @@ import Image from 'react-bootstrap/Image';
 import Navbar from 'react-bootstrap/Navbar';
 import Searchbar from './Searchbar';
 import Dropdown from 'react-bootstrap/Dropdown';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Popover from 'react-bootstrap/Popover';
 
 
 import Services from '../data/Data.json';
@@ -21,11 +23,32 @@ import { Bell, Envelope, PersonCircle, BellFill } from 'react-bootstrap-icons';
 export default function PoliNavbar() {
 
     const { avvisi, setAvvisi } = useContext(AvvisiContext);
-    
+
+    const handleNotificationClick = (notifica) => {
+        const updatedAvvisi = avvisi[0].filter((n) => n !== notifica);
+        setAvvisi([updatedAvvisi]);
+    }
+
+    const popover = (
+        <Popover id="popover-basic">
+            <Popover.Header as="h3">Ultime notifiche</Popover.Header>
+            <Popover.Body>
+                {avvisi[0].map((notifica) => (
+                    <div key={notifica.id} 
+                    onClick={() => handleNotificationClick(notifica)}
+                    className='mb-2'>
+                        <h6 className='text-style'>{notifica.title}</h6>
+                        <span className='click-notifica'>{notifica.body}</span>
+                    </div>
+                ))}
+            </Popover.Body>
+        </Popover>
+    );
+
     return (
         <Navbar className="custom-navbar">
             <Container fluid>
-                <Navbar.Brand className="d-none d-lg-block" as={Link} target='_blank' to="https://www.polito.it/" style={{ width: 'auto', minWidth:'166.3px', height: '57px', marginLeft: '-3px', marginRight: '36px' }}>
+                <Navbar.Brand className="d-none d-lg-block" as={Link} target='_blank' to="https://www.polito.it/" style={{ width: 'auto', minWidth: '166.3px', height: '57px', marginLeft: '-3px', marginRight: '36px' }}>
                     <Image
                         src={Logo}
                         alt="Logo PoliTo"
@@ -40,21 +63,21 @@ export default function PoliNavbar() {
                     />
                 </Navbar.Brand>
                 <Navbar.Brand className="d-none d-lg-block">
-                <span
-                style={{
-                    color: '#002B49',
-                    fontFamily: 'Montserrat, sans-serif',
-                    fontWeight:'600',
-                    display: 'inline-block',
-                    fontSize: '22px',
-                  }}
-                >
-                Portale della didattica
-                </span>
+                    <span
+                        style={{
+                            color: '#002B49',
+                            fontFamily: 'Montserrat, sans-serif',
+                            fontWeight: '600',
+                            display: 'inline-block',
+                            fontSize: '22px',
+                        }}
+                    >
+                        Portale della didattica
+                    </span>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll" className='justify-content-end'>
-                <Searchbar  services={Services} />
+                    <Searchbar services={Services} />
                     <Nav
                         className="my-0 my-lg-0"
                         style={{ maxHeight: '100px' }}
@@ -62,13 +85,17 @@ export default function PoliNavbar() {
                     >
                         <Nav.Link as={Link} to="https://mail.studenti.polito.it/?_task=mail&_mbox=INBOX" target='_blank' style={{ marginRight: '5px', marginTop: '9px' }}><Envelope size={28} color='#002B49' /></Nav.Link>
                         <Nav.Link as={Link} to="/" style={{ marginRight: '12px', marginTop: '9px' }}>
-                            {avvisi[0].length === 0 ?  <Bell size={28} color='#002B49' /> : <BellFill size={28} color='#002B49' />}
+                            {avvisi[0].length === 0 ? <Bell size={28} color='#002B49' /> :
+                                <OverlayTrigger trigger="click" placement="bottom" overlay={popover}>
+                                    <BellFill size={28} color='#002B49' />
+                                </OverlayTrigger>
+                            }
                         </Nav.Link>
                         <Navbar.Text className="text-style" style={{ fontWeight: '500', fontSize: '16px', color: '#002B49', marginRight: '12px' }}>
                             <div className='d-none d-md-block'>
-                            s123456
-                            <br />
-                            <span className='truncated'>Mario Rossi</span>
+                                s123456
+                                <br />
+                                <span className='truncated'>Mario Rossi</span>
                             </div>
                         </Navbar.Text>
                         <Navbar.Brand>
@@ -83,9 +110,9 @@ export default function PoliNavbar() {
                                     <PersonCircle size={48} color='#002B49' />
                                 </Dropdown.Toggle>
 
-                                <Dropdown.Menu style={{right: 'auto', left: '-120px', fontFamily:'Montserrat, sans-serif',}}>
-                                    <Dropdown.Item style={{fontWeight:'500'}}>Dati utente</Dropdown.Item>
-                                    <Dropdown.Item style={{fontWeight:'500'}}>Logout</Dropdown.Item>
+                                <Dropdown.Menu style={{ right: 'auto', left: '-120px', fontFamily: 'Montserrat, sans-serif', }}>
+                                    <Dropdown.Item style={{ fontWeight: '500' }}>Dati utente</Dropdown.Item>
+                                    <Dropdown.Item style={{ fontWeight: '500' }}>Logout</Dropdown.Item>
                                 </Dropdown.Menu>
 
                             </Dropdown>
