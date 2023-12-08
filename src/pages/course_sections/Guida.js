3889018def19d456c317201c10cb64a455ff1738
Gian114 <100781136+Gian114@users.noreply.github.com>
2023-12-08 17:44:46 +0100

    Guida done

--- src/pages/course_sections/Guida.js
+++ src/pages/course_sections/Guida.js
@@ -1,14 +1,209 @@
 import React from 'react';
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function Guida() {
 
     return (
         <>
             <Container className="custom-container m-0">
-               
+                <Row className='my-3 pt-3'>
+                    <Col md={3}>
+                        <h6 className="text-style" style={{ fontWeight: '600' }}>
+                            Anno accademico di inizio valdità
+                        </h6>
+                    </Col>
+                    <Col>
+                        <h6 className='text-style'>2023/2024</h6>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={3}>
+                        <h6 className="text-style" style={{ fontWeight: '600' }}>
+                            Presentazione
+                        </h6>
+                    </Col>
+                    <Col>
+                        <h6 className='text-style'>Il corso si propone di fornire le basi necessarie per l'interpretazione di fenomeni chimici, per la conoscenza della struttura e delle proprietà dei solidi cristallini e per la comprensione ed il calcolo dei fenomeni energetici relativi a sistemi chimici ed elettrochimici. L'insegnamento intende fornire allo studente una solida preparazione scientifica, volta alla conoscenza, alla comprensione e alla descrizione della struttura e delle proprietà della materia, contribuendo, contestualmente agli altri insegnamenti del primo anno, al consolidamento dei fondamenti culturali tecnico-scientifici propri di ogni ingegnere. L’insegnamento di Chimica costituisce pertanto uno strumento efficace per l’accrescimento sia della capacità di ragionamento critico che dell’uso appropriato del linguaggio scientifico.</h6>
+                    </Col>
+                </Row>
+                <Row className='my-3'>
+                    <Col md={3}>
+                        <h6 className="text-style" style={{ fontWeight: '600' }}>
+                            Risultati attesi
+                        </h6>
+                    </Col>
+                    <Col>
+                        <h6 className='text-style'>Al termine del corso lo studente dovrà essere in grado di: - conoscere e comprendere i principi fondamentali della chimica generale e inorganica; - comprendere la struttura e le proprietà dei solidi cristallini; - conoscere e comprendere i principi fondamentali della chimica organica; - comprendere i principi fondamentali della chimica fisica; - conoscere e comprendere i principi fondamentali della chimica analitica; - conoscere e comprendere i principi fondamentali della chimica industriale.</h6>
+                    </Col>
+                </Row>
+                <Row className='my-3'>
+                    <Col md={3}>
+                        <h6 className="text-style" style={{ fontWeight: '600' }}>
+                            Prerequisiti
+                        </h6>
+                    </Col>
+                    <Col>
+                        <h6 className='text-style'>Non sono previsti prerequisiti.</h6>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={3}>
+                        <h6 className="text-style" style={{ fontWeight: '600' }}>
+                            Programma del corso
+                        </h6>
+                    </Col>
+                    <Col>
+                        ATOMO:
+                        Costituzione del nucleo, struttura dell'atomo di idrogeno e modelli atomici di Bohr e quantistico-ondulatorio.
+                        Numeri quantici, principio di esclusione e regola di Hund
+                        Forma spaziale degli orbitali s,p,d<br></br>
+
+                        CLASSIFICAZIONE DEGLI ELEMENTI E TAVOLA PERIODICA:
+                        Configurazione elettronica degli elementi e periodicità nelle loro proprietà chimiche e fisiche
+                        Raggio atomico, energia di ionizzazione ed affinità elettronica.
+                        Suddivisione in metalli, nonmetalli e semimetalli.
+                        Generalità sui gruppi principali della tavola periodica e su alcuni metalli di transizione.<br></br>
+
+                        LEGAME CHIMICO:
+                        Il legame ionico, covalente, metallico.
+                        Legame semplice, doppio e triplo.
+                        Geometria molecolare, teoria dell'ibridazione e della risonanza.
+                        Cenni sulla teoria degli orbitali molecolari.<br></br>
+
+                        INTERAZIONI INTERMOLECOLARI:
+                        Interazioni di van der Waals e legame a idrogeno.<br></br>
+
+                        NOMENCLATURA, FORMULE E STECHIOMETRIA:
+                        Massa atomica e molecolare, mole, numero di Avogadro; definizioni ed unità di misura.
+                        Reazioni acido-base e formazione di sali.
+                        Stato di ossidazione e bilanciamento di reazioni redox.
+                        Relazioni ponderali in una reazione, reagente limitante.
+                        Reazioni di combustione.<br></br>
+
+                        STATO GASSOSO:
+                        Leggi dei gas ideali. Equazione di stato dei gas ideali.
+                        Distribuzione statistica delle energie (Boltzmann).
+                        Equazione di stato dei gas reali.<br></br>
+
+                        STATO LIQUIDO:
+                        Tensione di vapore di liquidi puri.
+                        Soluzioni e modi di esprimere la loro concentrazione. Dissociazione elettrolitica e grado di ionizzazione. Proprietà colligative di soluzioni elettrolitiche e non.
+                        Definizione di acido e base secondo Arrhenius, Brönsted e Lewis.
+                        Prodotto ionico dell'acqua. pH, pOH e idrolisi dei sali.<br></br>
+
+                        STATO SOLIDO:
+                        Cella elementare e struttura dei più semplici solidi cristallini. Reticoli cristallini. Classificazione dei solidi.<br></br>
+
+                        EQUILIBRI TRA FASI:
+                        Liquefazione dei gas.
+                        Diagramma di stato di sostanze pure (acqua e anidride carbonica).<br></br>
+
+                        CINETICA CHIMICA E CATALISI:
+                        Velocità e ordine di reazione. Equazione di Arrhenius. Energia di attivazione. Catalisi e catalizzatori.<br></br>
+
+                        EQUILIBRIO CHIMICO:
+                        Legge di azione di massa. Equilibri chimici omogenei ed eterogenei. Principio di le Chatelier ed influenza della temperatura sull'equilibrio.<br></br>
+
+                        TERMODINAMICA CHIMICA:
+                        Primo, secondo e terzo Principio. Entalpia di reazione e legge di Hess. Energia libera ed entropia in una reazione. Criterio di spontaneità.<br></br>
+
+                        ELETTROCHIMICA:
+                        Potenziale normale ed effettivo di un elettrodo. Celle elettrochimiche ed equazione di Nernst. Forza elettromotrice di una pila. Reattività di metalli con acidi ossidanti e non. Elettrolisi e leggi di Faraday.<br></br>
+
+                        CHIMICA ORGANICA:
+                        Nomenclatura e proprietà dei principali composti organici. I gruppi funzionali più comuni e le loro principali reazioni. Isomeria strutturale.<br></br>
+                    </Col>
+                </Row>
+                <Row className='my-3'>
+                    <Col md={3}>
+                        <h6 className="text-style" style={{ fontWeight: '600' }}>
+                            Sustainable development goals
+                        </h6>
+                    </Col>
+                    <Col>
+                        <h6 className='text-style'>4 - Quality Education</h6>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={3}>
+                        <h6 className="text-style" style={{ fontWeight: '600' }}>
+                            Organizzazione dell'insegnamento
+                        </h6>
+                    </Col>
+                    <Col>
+                        <h6 className='text-style'>
+                            L'insegnamento è strutturato in:<br></br><br></br>
+
+                            - 58.5 ore di lezioni di teoria in aula mirate allo sviluppo di conoscenze relative alla struttura e alle proprietà della materia, in un percorso che, partendo dalla descrizione della struttura atomica, giunge ad affrontare le diverse tipologie di legame chimico, le proprietà dei diversi stati di aggregazione, gli aspetti cinetici e termodinamici delle trasformazioni chimiche e fisiche, i fondamenti dell'elettrochimica.<br></br>
+
+                            - 18 ore di ESERCITAZIONI in Aula (studenti/esse suddivisi in due squadre). Durante le esercitazioni non vengono spiegati nuovi argomenti ma sono trattati gli aspetti numerici e computazionali di alcuni di essi, in particolare: ' Ripasso di nomenclatura inorganica. ' Mole, peso molecolare e peso molare. ' Relazioni ponderali nelle reazioni chimiche. ' Reazioni di ossido-riduzione e loro bilanciamento. ' Leggi dei gas ideali. ' Proprietà colligative delle soluzioni. ' Equilibri chimici omogenei ed eterogenei. ' Termochimica ' pH ed equilibri in soluzione acquosa ' Equazione di Nernst relativa ad un elettrodo o ad una cella. ' Leggi di Faraday.<br></br>
+
+                            - 3 ore di LABORATORIO didattico, in cui gli studenti/esse, suddivisi in squadre di 3-4 persone, svolgono sotto la guida di tutori alcuni semplici esperimenti didattici che favoriscano la comprensione e l'assimilazione delle nozioni di teoria affrontate durante il corso ' Cinetica di reazione ' Reazioni di precipitazione ' Titolazione acido-base ' Reazioni redox ' Misura del pH ' Pila Daniell. Il Laboratorio sperimentale è disponibile sia per gli studenti che frequentano in sede, sia per quelli che frequentano il primo anno SDSS presso la sede di Biella. Negli altri SDSS è sostituito da esercitazioni in aula.<br></br>
+
+                            - MODULO TUTORAGGIO E PROVE IN ITINERE. Parallelamente alle lezioni di teoria e alle esercitazioni è previsto un modulo di TUTORAGGIO, volto a consolidare le conoscenze e le competenze di base, nonché a favorire la partecipazione attiva dello/a studente/essa durante il semestre, e la graduale preparazione all’esame finale. A tal fine sono previste delle ATTIVITÀ IN ITINERE che consistono in due test con attribuzione di punteggio (a metà e a fine insegnamento, presso i Laib), ciascuno preceduto da un test preliminare (da remoto) utile allo/a studente/essa per prendere dimestichezza con la modalità d’esame basata su test a risposta multipla. La partecipazione ai test preliminari ed il superamento di entrambi i test valutati (che hanno struttura e punteggi analoghi a quelli del test d’esame) danno accesso diretto alla prova scritta d’esame (si veda la sezione modalità d’esame). Le modalità e le scadenze di tali prove saranno rese note all’inizio del corso e pubblicate nella pagina web del corso.<br></br>
+                        </h6>
+                    </Col>
+                </Row>
+                <Row className='my-3'>
+                    <Col md={3}>
+                        <h6 className="text-style" style={{ fontWeight: '600' }}>
+                            Bibliografia
+                        </h6>
+                    </Col>
+                    <Col>
+                        <h6 className='text-style'>
+                            I testi consigliati, scelti tra quelli elencati, saranno comunicati a lezione dal Docente titolare dell'insegnamento.<br></br><br></br>
+
+                            ' P. Atkins, L. Jones, Principi di Chimica, Zanichelli <br></br>
+                            ' Giannoccaro, Doronzo, Elementi di stechiometria, EdiSES<br></br>
+                            ' Manfredotti-Lanfredi, Tiripicchio, Fondamenti di Chimica, Casa Editrice Ambrosiana<br></br>
+                            ' R. Michelin A. Munari, Fondamenti di Chimica per l'ingegneria, Cedam<br></br>
+                            ' Petrucci-Harwood, Chimica generale,Principi e moderne applicazioni,Piccin<br></br>
+                            ' Schiavello, Palmisano, Fondamenti di Chimica, EdiSES<br></br>
+                            ' M.S.Silberberg, Chimica, Mc Graw-Hill<br></br><br></br>
+
+                            Materiale utile si trova anche sul sito della didattica online del Politecnico di Torino o nelle pagine web dei singoli corsi.
+                        </h6>
+                    </Col>
+                </Row>
+                <Row className='my-3'>
+                    <Col md={3}>
+                        <h6 className="text-style" style={{ fontWeight: '600' }}>
+                            Materiale di supporto allo studio
+                        </h6>
+                    </Col>
+                    <Col>
+                        <h6 className='text-style'>
+                            Lucidi delle lezioni; Libro di testo; Libro di esercitazione; Materiale multimediale ;
+                        </h6>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={3}>
+                        <h6 className="text-style" style={{ fontWeight: '600' }}>
+                            Criteri, regole e procedure per l'esame esclusivamente IN PRESENZA
+                        </h6>
+                    </Col>
+                    <Col>
+                        <h6 className='text-style'>
+                            Modalità di esame: Test informatizzato in laboratorio; Prova scritta (in aula); Prova orale facoltativa;<br></br><br></br>
+
+                            Exam: Computer lab-based test; Written test; Optional oral exam;<br></br>
+                            ...
+                            L'esame consiste in un test a domande multiscelta, seguito da una prova scritta obbligatoria e, se richiesta, da una prova orale. L’accesso alla prova scritta è vincolato dal superamento del test o di entrambe le prove in itinere.<br></br>
+
+                            Il test informatizzato ai Laib è finalizzato a verificare una preparazione di base minima necessaria per affrontare l'esame scritto. L’esame scritto vuole verificare le conoscenze di chimica generale inorganica e organica acquisite durante il corso e la capacità dello/a studente/essa di risolvere esercizi di calcolo stechiometrico su reazioni chimiche ed elettrochimiche, equazioni termochimiche, gas e soluzioni liquide. Non è consentito l’utilizzo di materiale didattico in nessuna prova.<br></br>
+
+                            Il test (così come le due prove in itinere con valutazione), della durata di 25 minuti, consiste in 15 domande a risposta multipla al calcolatore. Il punteggio massimo ottenibile al test è 9. Se il punteggio conseguito al test è inferiore a 6, l'esame viene registrato come respinto; altrimenti lo/a studente/essa prosegue il suo esame sostenendo una prova scritta della durata di 70 minuti, che consiste sia di esercizi, sia di domande teoriche. La prova scritta permette di conseguire un punteggio massimo di 22. Se il punteggio conseguito nella prova scritta è inferiore a 12, l'esame viene registrato come respinto.<br></br>
+
+                            Il voto finale è la somma dei voti del test (o la media delle due prove in itinere, che devono essere entrambe superate) e della prova scritta. Se il voto finale è pari o superiore a 18/30, lo/a studente/essa ha la possibilità di sostenere, se lo richiede, una prova orale aggiuntiva su tutto il programma svolto durante il corso. In tal caso il voto finale terrà conto anche della prova orale.<br></br>
+
+                            Inoltre, il/la docente ha comunque facoltà di convocare lo studente per una prova orale, a verifica dell’esame scritto.<br></br>
+                        </h6>
+                    </Col>
+                </Row>
             </Container>
         </>
-    );  
+    );
 
 }
