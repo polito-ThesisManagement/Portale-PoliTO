39b7bc3ef8ecc3efbb810a79fe1e08a3eb1dd306
SYLVIE MOLINATTO s318952 <s318952@studenti.polito.it>
2024-11-12 15:39:15 +0100

    refactor: remove unused ESLint configuration and dependencies

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   projectId: 'twh9wt',
   e2e: {
-    /* eslint-disable no-unused-vars */
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
