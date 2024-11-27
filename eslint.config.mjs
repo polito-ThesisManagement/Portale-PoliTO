6a56a1dc8d14013b5e10a9bd51d54c69a81cd7ec
SYLVIE MOLINATTO s318952 <s318952@studenti.polito.it>
2024-11-27 15:19:08 +0100

    refactor: add eslint-plugin-unused-imports and update rules for better import management

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,8 @@ import pluginJsxA11y from 'eslint-plugin-jsx-a11y';
 import pluginPrettier from 'eslint-plugin-prettier';
 import pluginReact from 'eslint-plugin-react';
 import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginUnusedImports from 'eslint-plugin-unused-imports';
+
 import globals from 'globals';
 
 /** @type {import('eslint').Linter.Config[]} */
@@ -36,19 +38,26 @@ export default [
       prettier: pluginPrettier,
       react: pluginReact,
       'react-hooks': pluginReactHooks,
+      'unused-imports': pluginUnusedImports,
     },
     rules: {
+      //'import/no-unresolved': 'error', // Ensure all imports are resolved
+      'import/named': 'error', // Ensure named imports correspond to named exports
       'jsx-a11y/alt-text': 'warn',
       //'jsx-a11y/click-events-have-key-events': 'warn',
       //'jsx-a11y/label-has-associated-control': 'error',
       //'jsx-a11y/no-static-element-interactions': 'warn',
+      //'no-console': ['warn', { allow: ['warn', 'error'] }], // Allow warnings and errors, but not other console methods
+      'no-debugger': 'warn',
       'no-unused-vars': 'off',
+      'react/display-name': 'warn', // Enforce display names for React components
       //'react/jsx-boolean-value': ['warn', 'never'], // Avoid unnecessary boolean values in JSX props
       'react/jsx-key': 'error', // Enforce keys for array elements
       'react/jsx-no-duplicate-props': 'error', // Avoid duplicate props in JSX
       //'react/jsx-no-useless-fragment': 'warn',      // Prevent useless fragments
       'react/jsx-pascal-case': 'warn', // Enforce PascalCase for component names
       'react/jsx-uses-react': 'off',
+      'react/jsx-uses-vars': 'warn', // Ensure JSX variables are marked as used
       //'react/no-array-index-key': 'warn',           // Discourage using array index as key
       'react/no-unknown-property': 'warn', // Prevent invalid DOM properties
       'react/prop-types': 'warn',
@@ -56,6 +65,7 @@ export default [
       //'react-hooks/exhaustive-deps': 'warn', // Validates dependencies of hooks
       //'react-hooks/rules-of-hooks': 'error', // Ensures hooks are used correctly
       'prettier/prettier': 'error',
+      //"unused-imports/no-unused-imports": "warn", // Warn about unused imports
     },
     settings: {
       react: {
