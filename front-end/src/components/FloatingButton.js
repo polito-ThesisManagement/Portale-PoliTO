1c12c33cb66f9b553a4fe23c58405bf73498fd05
Luca Barbato <luca.barbato@studenti.polito.it>
2024-12-03 23:49:58 +0100

    feat: floating button added

---
+++ front-end/src/components/FloatingButton.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+
+import { Button } from 'react-bootstrap';
+
+import { scrollTop } from '../utils/utils';
+
+import { FaArrowUp } from "react-icons/fa6";
+
+import '../styles/Utilities.css';
+
+function FloatingButton() {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const handleScroll = () => {
+        if (window.scrollY > 100)
+            setIsVisible(true)
+        else
+            setIsVisible(false);
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    return (
+        <Button className="floating-button" onClick={scrollTop} style={{ display: isVisible ? "flex" : "none" }}>
+            <FaArrowUp size={16} />
+        </Button>
+    );
+}
+
+export default FloatingButton;
