6f27cd31d90029c1d463b8160d57f8cc3ce329ce
SYLVIE MOLINATTO s318952 <s318952@studenti.polito.it>
2024-12-03 12:07:11 +0100

    feat: add attachment_url and profile_picture_url to teacher and thesis proposal attributes

--- back-end/src/utils/selectTeacherAttributes.js
+++ back-end/src/utils/selectTeacherAttributes.js
@@ -2,7 +2,7 @@ const selectTeacherAttributes = (detailed = false) => {
   const attributes = ['id', 'first_name', 'last_name'];
 
   if (detailed) {
-    attributes.push('role', 'email', 'profile_url', 'facility_short_name');
+    attributes.push('role', 'email', 'profile_url', 'profile_picture_url', 'facility_short_name');
   }
   return attributes;
 };
