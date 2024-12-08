f6492a4f62bc67d1eb77d91c4b05739501617677
SYLVIE MOLINATTO s318952 <s318952@studenti.polito.it>
2024-12-08 18:26:05 +0100

    feat: add new API endpoints for thesis proposal types, keywords, and teachers

--- back-end/src/routers/thesis-proposals.js
+++ back-end/src/routers/thesis-proposals.js
@@ -3,11 +3,17 @@ const router = express.Router();
 const {
   getThesisProposals,
   getTargetedThesisProposals,
+  getThesisProposalsTypes,
+  getThesisProposalKeywords,
+  getThesisProposalTeachers,
   getThesisProposalById,
 } = require('../controllers/thesis-proposals');
 
 router.get('/', getThesisProposals);
 router.get('/targeted', getTargetedThesisProposals);
+router.get('/types', getThesisProposalsTypes);
+router.get('/keywords', getThesisProposalKeywords);
+router.get('/teachers', getThesisProposalTeachers);
 router.get('/:thesisProposalId', getThesisProposalById);
 
 module.exports = router;
