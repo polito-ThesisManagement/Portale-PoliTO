c26ce134061f7a42e729a3180b728ba237733e3d
SYLVIE MOLINATTO s318952 <s318952@studenti.polito.it>
2024-12-13 17:06:57 +0100

    fix: error on merge

--- front-end/src/pages/carriera/PropostaDiTesi.js
+++ front-end/src/pages/carriera/PropostaDiTesi.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+import { useTranslation } from 'react-i18next';
+import { useParams } from 'react-router-dom';
+
+import API from '../../API';
+import LoadingModal from '../../components/LoadingModal';
 import MyBreadcrumb from '../../components/MyBreadcrumb';
-import ThesisProposals from '../../components/ThesisProposals';
+import { ThesisProposalDetail } from '../../components/ThesisProposalDetail';
 
-export default function ProposteDiTesi() {
+function PropostaDiTesi() {
+  const id = useParams().id;
+  const { i18n } = useTranslation();
+  const [thesisProposal, setThesisProposal] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    API.getThesisProposalById(id, i18n.language)
+      .then(data => {
+        setThesisProposal(data);
+      })
+      .catch(error => console.error('Error fetching thesis proposal by ID:', error))
+      .finally(() => setLoading(false));
+  }, [id, i18n.language]);
+
   return (
     <>
-      <MyBreadcrumb />
-      <ThesisProposals />
+      {loading ? (
+        <LoadingModal show={loading} onHide={() => setLoading(false)} />
+      ) : (
+        <>
+          <MyBreadcrumb />
+          <ThesisProposalDetail id={id} thesisProposal={thesisProposal} />
+        </>
+      )}
     </>
   );
 }
+
+export default PropostaDiTesi;
