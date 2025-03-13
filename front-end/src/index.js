e578c914203366149238728334e70ee402cdea5a
SYLVIE MOLINATTO s318952 <s318952@studenti.polito.it>
2025-03-13 19:16:32 +0100

    refactor: update Bootstrap CSS import

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,8 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.css';
 
 import App from './App';
 import './i18n';
